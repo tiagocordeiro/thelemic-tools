4dbac46d3112f46b6c219492431f84fde1ede450
Tiago Cordeiro <tiago@mulhergorila.com>
2019-10-08 15:48:27 -0300

    Configura testes simples.
    Ajustes de código PEP8
    
    This close #8

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from django.test import TestCase, Client, RequestFactory
+
+from .views import index
+
 
 # Create your tests here.
+class IndexViewTest(TestCase):
+    def setUp(self) -> None:
+        self.client = Client()
+        self.factory = RequestFactory()
+
+    def test_index_view(self):
+        request = self.factory.get('/')
+
+        response = index(request)
+        self.assertEqual(response.status_code, 200)
