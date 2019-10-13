98a52b9463185d2324d1488ac8a53121d3a70648
Tiago Cordeiro <tiago@mulhergorila.com>
2019-10-13 19:36:16 -0300

    Adiciona local customizado para o app Resh.

--- resh/admin.py
+++ resh/admin.py
@@ -1,3 +1,11 @@
-# from django.contrib import admin
+from django.contrib import admin
+
+from .models import CustomLocation
+
 
 # Register your models here.
+class CustomLocationAdmin(admin.ModelAdmin):
+    list_display = ('name', )
+
+
+admin.site.register(CustomLocation, CustomLocationAdmin)
--- resh/urls.py
+++ resh/urls.py
@@ -3,5 +3,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('hours/', views.resh_hours_list, name='resh_hours_list'),
+    path('hours/',
+         views.resh_hours_list,
+         name='resh_hours_list'),
+    path('hours/<int:location_id>/',
+         views.resh_hours_list_saved_location,
+         name='resh_hours_list_saved_location'),
+    path('hours/<name>/<region>/<latitude>/<longitude>/<time_zone>/<elevation>',
+         views.resh_hours_list_custom_location,
+         name='resh_hours_list_custom_location')
 ]
