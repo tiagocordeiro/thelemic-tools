3342c66f81f06ecf8a565837c0cf6418ee49949f
Tiago Cordeiro <tiago@mulhergorila.com>
2019-10-13 19:36:16 -0300

    Adiciona local customizado para o app Resh.

--- resh/models.py
+++ resh/models.py
@@ -1,3 +1,16 @@
-# from django.db import models
+from django.db import models
+
 
 # Create your models here.
+class CustomLocation(models.Model):
+    name = models.CharField('Local', max_length=50)
+    region = models.CharField('Região', max_length=20)
+    latitude = models.CharField('Latitude', max_length=20)
+    longitude = models.CharField('Longitude', max_length=20)
+    time_zone = models.CharField('Timezone', max_length=50)
+    elevation = models.CharField('Elevação', max_length=20)
+
+    class Meta:
+        ordering = ('name', 'region')
+        verbose_name_plural = "locais"
+        verbose_name = "local"
--- resh/views.py
+++ resh/views.py
@@ -1,11 +1,36 @@
 from django.shortcuts import render
 
 from .facade import resh_hours
+from .models import CustomLocation
 
 
 # Create your views here.
 def resh_hours_list(request):
     horario_pratica = resh_hours("Santo André, SP", "Brasil", -23.63434, -46.53395, "America/Sao_Paulo", 756)
+    context = {'ra': horario_pratica['Nascer do sol'].strftime("%H:%M:%S"),
+               'ahathoor': horario_pratica['Meio-dia solar'].strftime("%H:%M:%S"),
+               'tum': horario_pratica['Pôr do sol'].strftime("%H:%M:%S"),
+               'kephera': horario_pratica['Meia-noite solar'].strftime("%H:%M:%S")}
+    return render(request, 'resh/hours.html', {'context': context})
+
+
+def resh_hours_list_saved_location(request, location_id):
+    custom_local = CustomLocation.objects.get(pk=location_id)
+    horario_pratica = resh_hours(local_name=custom_local.name, region=custom_local.region,
+                                 latitude=float(custom_local.latitude), longitude=float(custom_local.longitude),
+                                 time_zone=custom_local.time_zone, elevation=int(custom_local.elevation))
+    context = {'ra': horario_pratica['Nascer do sol'].strftime("%H:%M:%S"),
+               'ahathoor': horario_pratica['Meio-dia solar'].strftime("%H:%M:%S"),
+               'tum': horario_pratica['Pôr do sol'].strftime("%H:%M:%S"),
+               'kephera': horario_pratica['Meia-noite solar'].strftime("%H:%M:%S")}
+    return render(request, 'resh/hours.html', {'context': context})
+
+
+def resh_hours_list_custom_location(request, name, region, latitude, longitude, time_zone, elevation):
+    tzfix = time_zone.replace('-', '/')
+    horario_pratica = resh_hours(local_name=name, region=region,
+                                 latitude=float(latitude), longitude=float(longitude),
+                                 time_zone=tzfix, elevation=int(elevation))
     context = {'ra': horario_pratica['Nascer do sol'].strftime("%H:%M:%S"),
                'ahathoor': horario_pratica['Meio-dia solar'].strftime("%H:%M:%S"),
                'tum': horario_pratica['Pôr do sol'].strftime("%H:%M:%S"),
