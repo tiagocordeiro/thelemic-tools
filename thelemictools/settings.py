ee8336dd37bcc31c09d965ed7b717c7974bcec16
Tiago Cordeiro <tiago@mulhergorila.com>
2021-05-12 14:29:02 -0300

    Update Dependencies
    - Atualiza dependencias
    - Upgrading to Django 3.2 and fixing DEFAULT_AUTO_FIELD warnings
    
    This close #145

--- thelemictools/settings.py
+++ thelemictools/settings.py
@@ -84,6 +84,9 @@ DATABASES = {
     'default': config('DATABASE_URL', default=default_dburl, cast=dburl),
 }
 
+# Required by Django > 3.2
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
@@ -162,4 +165,4 @@ if CLOUDINARY_URL:  # pragma: no cover
     COLLECTFAST_ENABLED = False
 
     DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.RawMediaCloudinaryStorage'
-    STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
+    STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticCloudinaryStorage'
